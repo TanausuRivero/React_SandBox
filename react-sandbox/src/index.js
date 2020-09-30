0820460fcca331fbe228d81c5b06d0a2770159ad
TanausuRivero <hobdur@gmail.com>
2020-09-30 18:40:01 +0100

    Tutorial wSchool react
    https://www.w3schools.com/react/react_lifecycle.asp

--- react-sandbox/src/index.js
+++ react-sandbox/src/index.js
@@ -1,17 +1,51 @@
+
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+class Car extends React.Component {
+  constructor(props){
+    super(props);
+    this.state={brand: "ford",
+                color: "red",
+                size: "3,30m"}
+  }
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+  changeColor = () => {
+    this.setState({brand: "seat"});
+    this.setState({color: "blue"});
+    this.setState({size: "10m"});
+  }
+
+  render() {
+     return (
+      <div>
+        <h1>My car is a {this.state.size} {this.state.brand}</h1>
+        <p>
+          Its a {this.state.color} car
+        </p>
+        <button
+          type="button"
+          onClick={this.changeColor}
+        >Change car</button>
+      </div>
+    );
+  }
+}
+
+class Garage extends React.Component {
+  render() {
+    const carinfo = {name: "Ford", model: "Mustang"};
+    return (
+      <div>
+      <h1>Who lives in my garage?</h1>
+      <Car name={this.props.name} model={this.props.model} />
+      </div>
+    );
+  }
+}
+const myelement = <Car param1="Ford" param2="3"/>;
+
+ReactDOM.render(<Car  name="Ford" model="Mustang"/>, document.getElementById('root'));
+
+// ReactDOM.render(<Garage />, document.getElementById('root'));
+//ReactDOM.render(<Car />, document.getElementById('inner'));
